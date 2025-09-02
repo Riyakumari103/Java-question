d3bc7de3a3225ee85d8ac95edab7c99a7392f823
Riya U. <riya@example.com>
2025-09-03 03:11:32 +0530

    Add Assignment on methods

--- level1/KmToMilesFixed.java
+++
@@ -1,7 +0,0 @@
-﻿public class KmToMilesFixed {
-    public static void main(String[] args) {
-        double km = 10.8;
-        double miles = km / 1.6;
-        System.out.printf(\"The distance %.2f km in miles is %.2f%n\", km, miles);
-    }
-}
